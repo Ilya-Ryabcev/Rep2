56deb10ad07c27f0b6d3d8cf4e79d84e2cf40d46
Илья <bull.81@list.ru>
2025-03-08 21:50:15 +0300

    Сохраняй main

--- main_copy1.py
+++ main_copy1.py
@@ -1,20 +1,3 @@
-def fanc_names1(lst: list):
-    for i, el in enumerate(lst):
-        print(lst[i])
-my_list1 = ["Леха", "Ярик", "Марат"]
-fanc_names1(my_list1)
-
-
-
-def fanc_names2(lst: list):
-    for el in lst:
-        print(el)
-my_list2 = ["Леха", "Ярик", "Марат"]
-fanc_names2(my_list2)
-
-
-def func_autos(lst: list):
-    for i in lst:
-        print(f"Я хочу купить {i}")
-list_autos = ["hyunday", "bmw", "chevrolet"]
-func_autos(list_autos)
+a=1
+b=2
+b=3
