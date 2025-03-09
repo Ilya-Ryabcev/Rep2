63090476a138eff5a7a524a0f98b20b565f7bfd7
Илья <bull.81@list.ru>
2025-03-09 12:44:12 +0300

    Сохраняй copy1

--- main_copy1.py
+++ main_copy1.py
@@ -1,17 +1,30 @@
-def fanc_names1(lst: list):
-    for i, el in enumerate(lst):
-        print(lst[i])
-my_list1 = ["Леха", "Ярик", "Марат"]
-fanc_names1(my_list1)
+# def fanc_names1(lst: list):
+#     for i, el in enumerate(lst):
+#       print(lst[i])
+# my_list1 = ["Леха", "Ярик", "Марат"]
+# fanc_names1(my_list1)
+#
+# def fanc_names2(lst: list):
+#     for el in lst:
+#         print(el)
+# my_list2 = ["Леха", "Ярик", "Марат"]
+# fanc_names2(my_list2)
+#
+# def func_autos(lst: list):
+#     for i in lst:
+#         print(f"Я хочу купить {i}")
+# list_autos = ["hyunday", "bmw", "chevrolet"]
+# func_autos(list_autos)
 
-def fanc_names2(lst: list):
-    for el in lst:
-        print(el)
-my_list2 = ["Леха", "Ярик", "Марат"]
-fanc_names2(my_list2)
 
-def func_autos(lst: list):
-    for i in lst:
-        print(f"Я хочу купить {i}")
-list_autos = ["hyunday", "bmw", "chevrolet"]
-func_autos(list_autos)
+def fanc_list(num: int):
+    list_guests = [input("Enter name: ") for x in range(num)]
+    print(list_guests)
+
+    def fanc_guests(lst: list):
+        for i in lst:
+            print(f"Приглашаю вас {i:.^10} на обед")
+
+    fanc_guests(list_guests)
+
+fanc_list(3)
