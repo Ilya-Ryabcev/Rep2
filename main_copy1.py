9acddee6dd4b95161ca77d6f9dc89f1a63df7b75
Илья <bull.81@list.ru>
2025-03-11 23:24:17 +0300

    Сохраняй main

--- main_copy1.py
+++ main_copy1.py
@@ -29,23 +29,23 @@
 #
 # fanc_list(3)
 
-def fanc_list(num):
-    i = 0
-    list_guests = [input(f"Введите имя {i+1:.^5} гостя: ") for i in range(num)]
-    print(list_guests)
-
-    def fanc_guests(lst: list):
-        for i in lst:
-            print(f"Приглашаю тебя {i:.>10} на обед")
-
-    fanc_guests(list_guests)
-
-    guest_minus = list_guests[1]
-    print(f"{guest_minus:.^10} не сможет прийти на обед")
-    list_guests.remove(guest_minus)
-    guest_new = input("Введите имя нового гостя: ")
-    print(f"{guest_new:.^10} прийдет на обед вместо {guest_minus:.^10}")
-    list_guests.append(guest_new)
-    print(f"Новый лист гостей {list_guests}")
-
-fanc_list(3)
+# def fanc_list(num):
+#     i = 0
+#     list_guests = [input(f"Введите имя {i+1:.^5} гостя: ") for i in range(num)]
+#     print(list_guests)
+#
+#     def fanc_guests(lst: list):
+#         for i in lst:
+#             print(f"Приглашаю тебя {i:.>10} на обед")
+#
+#     fanc_guests(list_guests)
+#
+#     guest_minus = list_guests[1]
+#     print(f"{guest_minus:.^10} не сможет прийти на обед")
+#     list_guests.remove(guest_minus)
+#     guest_new = input("Введите имя нового гостя: ")
+#     print(f"{guest_new:.^10} прийдет на обед вместо {guest_minus:.^10}")
+#     list_guests.append(guest_new)
+#     print(f"Новый лист гостей {list_guests}")
+#
+# fanc_list(3)
